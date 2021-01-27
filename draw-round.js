5fef754b18da460fb323f6afb1193cd2f7ba79bf
David Meir-Levy <davidmeirlevy@gmail.com>
2021-01-27 15:14:36 +0200

    wip

--- draw-round.js
+++ draw-round.js
@@ -9,7 +9,7 @@ async function checkDrawRound(req, res, next) {
 		}
 		next();
 	} catch (e) {
-		console.log('failed to load current round');
+		console.log('failed to load current round', e);
 		res.status(500).end();
 	}
 }
